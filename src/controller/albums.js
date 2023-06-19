94efb204f3bf4215ab40dc5cd443d02f3b3e7d8a
Duong_va_nhung_nguoi_ban <nguyenbinhduong200209@gmail.com>
2023-06-19 21:43:03 +0700

    albums

--- src/controller/albums.js
+++ src/controller/albums.js
@@ -18,20 +18,21 @@ const albumsController = {
         const friendList = await Friends.find({
           $or: [{ userId1: foundUser._id }, { userId2: foundUser._id }],
         })
-          .populate('userId1', 'firstname lastname ')
-          .populate('userId2', 'firstname lastname ')
+          .populate('userId1', 'firstname lastname')
+          .populate('userId2', 'firstname lastname')
           .exec();
+        console.log(friendList);
         const filteredFriendList = friendList.map(friend => {
           if (friend.userId1._id.toString() === foundUser._id.toString()) {
-            return friend.userId2;
+            return friend.userId2._id; // Thay đổi thành friend.userId2._id
           } else {
-            return friend.userId1;
+            return friend.userId1._id; // Thay đổi thành friend.userId1._id
           }
         });
-        newShareList = [...filteredFriendList, ...shareList];
+        newShareList = [...filteredFriendList, ...(shareList || [])]; // Thêm một mảng rỗng nếu shareList là null hoặc undefined
         break;
       case 'onlyme':
-        newShareList = [userId];
+        newShareList = [foundUser._id];
         break;
       case 'public':
         newShareList = []; // hoặc có thể gán newShareList = newMemberList nếu muốn chia sẻ công khai với tất cả mọi người
@@ -46,7 +47,6 @@ const albumsController = {
       vacationId,
       userId: foundUser._id,
       title,
-
       createdAt: new Date(),
       shareStatus,
       shareList: newShareList,
@@ -54,68 +54,90 @@ const albumsController = {
     });
 
     //Send to front
-    return res.status(201).json({ data: newAlbum, message: 'alblums created' });
+    return res.status(201).json({ data: newAlbum, message: 'Alblums created' });
   }),
   updateAlbum: asyncWrapper(async (req, res) => {
-    try {
-      const { albumId } = req.params;
-      const { title, description } = req.body;
+    const { title, description, shareStatus, albumId } = req.body;
+    const foundUser = req.userInfo;
+    // console.log(foundUser);
 
-      // Kiểm tra xem album có tồn tại trong database không
-      const existingAlbum = await Albums.findById(albumId);
-      if (!existingAlbum) {
-        return res.status(404).json({ message: 'Album not found' });
-      }
-
-      // Kiểm tra quyền truy cập của người dùng
-      if (existingAlbum.userId.toString() !== req.userInfo._id.toString()) {
-        return res.status(403).json({ message: 'Access denied' });
-      }
-
-      // Cập nhật thông tin album
-      existingAlbum.title = title;
-      existingAlbum.description = description;
-      existingAlbum.lastUpdateAt = new Date();
-
-      // Lưu thay đổi vào database
-      const updatedAlbum = await existingAlbum.save();
-
-      return res.status(200).json({
-        success: true,
-        message: 'Album updated',
-        album: updatedAlbum,
-      });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ success: false, message: 'Server error' });
+    // Kiểm tra xem album có tồn tại trong database không
+    const existingAlbum = await Albums.findOne(albumId);
+    console.log(existingAlbum.userId);
+    if (!existingAlbum) {
+      return res.status(404).json({ message: 'Album not found' });
     }
+    if (existingAlbum.userId.toString() !== foundUser._id.toString()) {
+      return res.status(401).json({ message: 'You are not authorized to update this album' });
+    }
+
+    let updateShareList;
+
+    switch (shareStatus) {
+      case 'protected':
+        const friendList = await Friends.find({
+          $or: [{ userId1: foundUser }, { userId2: foundUser }],
+        })
+          .populate('userId1', 'firstname lastname')
+          .populate('userId2', 'firstname lastname')
+          .exec();
+        const filteredFriendList = friendList.map(friend => {
+          if (friend.userId1._id.toString() === foundUser.toString()) {
+            return friend.userId2._id;
+          } else {
+            return friend.userId1._id;
+          }
+        });
+        updateShareList = [...filteredFriendList];
+        break;
+      case 'onlyme':
+        updateShareList = [foundUser];
+        break;
+      case 'public':
+        updateShareList = [];
+        break;
+      default:
+        updateShareList = null;
+        break;
+    }
+
+    // Cập nhật thông tin album
+    existingAlbum.shareStatus = shareStatus;
+    existingAlbum.shareList = updateShareList;
+    existingAlbum.title = title;
+    existingAlbum.description = description;
+    existingAlbum.lastUpdateAt = new Date();
+
+    // Lưu thay đổi vào database
+    const updatedAlbum = await existingAlbum.save();
+
+    return res.status(200).json({
+      message: 'Album updated',
+      album: updatedAlbum,
+    });
   }),
+
   deleteAlbum: asyncWrapper(async (req, res) => {
-    try {
-      const { albumId } = req.params;
+    const { albumId } = req.params;
+    const foundUser = req.userInfo;
+    // Kiểm tra xem album có tồn tại trong database không
+    const existingAlbum = await Albums.findOne(albumId);
 
-      // Kiểm tra xem album có tồn tại trong database không
-      const existingAlbum = await Albums.findById(albumId);
-      if (!existingAlbum) {
-        return res.status(404).json({ message: 'Album not found' });
-      }
-
-      // Kiểm tra quyền truy cập của người dùng
-      if (existingAlbum.userId.toString() !== req.userInfo._id.toString()) {
-        return res.status(403).json({ message: 'Access denied' });
-      }
-
-      // Xóa album khỏi database
-      await existingAlbum.remove();
-
-      return res.status(200).json({
-        success: true,
-        message: 'Album deleted',
-      });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ success: false, message: 'Server error' });
+    if (!existingAlbum) {
+      return res.status(404).json({ message: 'Album not found' });
     }
+
+    // Kiểm tra quyền truy cập của người dùng
+    if (existingAlbum.userId.toString() !== foundUser._id.toString()) {
+      return res.status(401).json({ message: 'You are not authorized to update this albums' });
+    }
+
+    // Xóa album khỏi database
+    await Albums.findByIdAndDelete(existingAlbum._id);
+
+    return res.status(200).json({
+      message: 'Albums deleted',
+    });
   }),
 };
 export default albumsController;
