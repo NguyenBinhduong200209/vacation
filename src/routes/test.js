5a2308d4f28ef22e446361d4efc77a73928105e2
nguyencongson96 <chansen2912@gmail.com>
2023-06-28 16:23:24 +0700

    update

--- src/routes/test.js
+++ src/routes/test.js
@@ -9,6 +9,7 @@ import Users from '#root/model/user/users';
 import Resources from '#root/model/resource';
 
 const monitor = asyncWrapper(async (req, res) => {
+  const { id } = req.query;
   const files = await fs.promises.readdir(path.join(resourcePath, id || ''));
   return res.status(200).json(files);
 });
