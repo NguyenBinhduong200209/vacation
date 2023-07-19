7139d712348ac7799bdc336364318f269a5e8e3e
nguyencongson96 <chansen2912@gmail.com>
2023-07-19 20:50:47 +0700

    update userinfo

--- src/routes/user/userinfor.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import usersinforController from '#root/controller/user/userinfo';
-
-const router = express.Router();
-
-router.get('/:id?', usersinforController.getfriendprofile);
-
-export default router;
